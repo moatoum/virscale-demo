9a448bf68835fdfab0ba5e354ab751629213b22c
Mohammed Atoum <atoum@Mohammeds-MacBook-Pro.local>
2025-10-01 00:21:51 +0300

    Fix Vercel API handler path

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 import { createRequestHandler } from '@remix-run/vercel';
 
 export default createRequestHandler({
-  build: () => import('../build/server/index.js'),
+  build: () => import('../build/server/build/server/index.js'),
   mode: process.env.NODE_ENV,
 });
