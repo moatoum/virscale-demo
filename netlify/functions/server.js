54710159d078f507111cdf3c364f33e400e7c25c
Mohammed Atoum <atoum@Mohammeds-MacBook-Pro.local>
2025-10-01 01:18:39 +0300

    Simplify Netlify function and include build files

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,13 +1,6 @@
-const path = require('path');
 const { createRequestHandler } = require('@netlify/remix-adapter');
 
-const BUILD_PATH = path.resolve(__dirname, '../../build/server');
-
-function getBuild() {
-  return require(BUILD_PATH);
-}
-
 exports.handler = createRequestHandler({
-  build: getBuild,
+  build: require('../../build/server'),
   mode: process.env.NODE_ENV || 'production',
 });
